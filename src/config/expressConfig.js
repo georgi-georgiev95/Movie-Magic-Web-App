c3b8c1baf7f1978e442aa9691d603ea6928b838a
Georgi957 <gesh007@abv.bg>
2024-01-17 19:14:22 +0200

    Extract router

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const handlebars = require('./handlebarsConfig');
+const router = require('../router');
 
 const app = express();
 handlebars(app);
 app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(router);
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const app = require('./config/expressConfig');
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
 
 app.listen(PORT, () => `Server is listening on port: ${PORT}`);
 
