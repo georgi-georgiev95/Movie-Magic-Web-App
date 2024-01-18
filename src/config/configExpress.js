77e600af0cfe7c01dc2d6918435a34cdbd64061a
Georgi957 <gesh007@abv.bg>
2024-01-18 19:54:29 +0200

    Install mongoose

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const router = require('../router');
+
+mongoose.connect('mongodb://127.0.0.1:27017/movie_magic')
+    .then(() => console.log("Connected to DB"))
+    .catch(err => console.log(err));
+
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, '../public')));
