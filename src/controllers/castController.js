c73e7703687c1b5cbfebf41e2c5a97bd6201accc
Georgi957 <gesh007@abv.bg>
2024-01-24 20:04:25 +0200

    Save cast to DB

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 
+const castService = require('../services/castService')
+
 router.get('/create', (req, res) => {
     res.render('casts/create');
 });
 
 router.get('/attach', (req, res) => {
     res.render('casts/attach');
+});
+
+router.post('/create', async (req, res) => {
+    const castData = req.body;
+    await castService.create(castData);
+    res.redirect('/')
 })
 
 module.exports = router;
--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -18,12 +18,12 @@ router.post('/create', async (req, res) => {
         res.redirect('movies/create');
     }
 
-    
+
 })
 
 router.get('/search', async (req, res) => {
     const movies = movieService.getAll();
-    res.render('movies/search', {movies});
+    res.render('movies/search', { movies });
 });
 
 router.post('/search', async (req, res) => {
@@ -36,20 +36,20 @@ router.post('/search', async (req, res) => {
         console.log(err.message);
         res.redirect('movies/search');
     }
-    
+
 });
 
 
 router.get('/details/:movieId', async (req, res) => {
     const id = req.params.movieId;
-    try {
-        const movie = await movieService.getOne(id).lean();
-        const movieData = ratingHelper(movie);
-        res.render('movies/details', { movieData });
-    } catch (err) {
-        console.log(err.message);
-    }
-    
+
+    const movie = await movieService.getOne(id).lean();
+    const movieData = ratingHelper(movie);
+    res.render('movies/details', { movieData });
+
+    console.log(err.message);
+
+
 });
 
 module.exports = router;
