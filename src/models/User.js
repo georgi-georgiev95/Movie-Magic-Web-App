222d49350623795b5dee4b0d08b51f468e17ca62
Georgi957 <gesh007@abv.bg>
2024-01-28 10:45:37 +0200

    Add user model

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.virtual('rePassword')
+    .set(function (value) {
+        if (value !== this.password) {
+            throw new mongoose.Error('Passwords must match!');
+        }
+    });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
