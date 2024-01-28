f51c7f1274869d6e48edcd6a471293d48c782fc1
Georgi957 <gesh007@abv.bg>
2024-01-28 11:11:44 +0200

    Use auth middleware

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const router = require('../router');
 const cookieParser = require('cookie-parser');
+const { auth } = require('../middlewares/authMiddleware');
 
 mongoose.connect('mongodb://127.0.0.1:27017/movie_magic')
     .then(() => console.log("Connected to DB"))
@@ -13,6 +14,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(auth);
 app.use(router);
 
 module.exports = app;
